3e40885000270215d469c378c1e9e122f8a7296d
Jacob Shirley <auriferous95@gmail.com>
2016-12-14 23:22:36 +0000

    Forgot to make renderer.js es6.

--- src/three/renderer.js
+++ src/three/renderer.js
@@ -1,91 +1,84 @@
-function Renderer() {
-	this.orbitControls = null;
-	this.scale = 1;
-	//add object
-	//remove object
-	//
-	//raycastObjects
-	//
-	//render
-	//
-	//getFPS
-	//
-}
-
-Renderer.prototype.init = function() {
-	this.meshes = [];
-
-	this.raycaster = new THREE.Raycaster();
-	this.scene = new THREE.Scene();
-
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000*this.scale);
-    this.camera.position.x = 3*this.scale;
-    this.camera.position.y = 3*this.scale;
-    this.camera.position.z = 3*this.scale;
-
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    document.body.appendChild(this.renderer.domElement);
-}
-
-Renderer.prototype.destroy = function() {
-
-}
-
-Renderer.prototype.addObject = function(object) {
-	if (object instanceof THREE.Light) {
-		this.scene.add(object);
-	} else {
-		this.meshes.push(object.renderData.mesh);
-		this.scene.add(object.renderData.mesh);
+class Renderer {
+	constructor() {
+		this.orbitControls = null;
+		this.scale = 1;
 	}
-}
 
-Renderer.prototype.removeObject = function(object) {
+	init() {
+		this.meshes = [];
 
-}
+		this.raycaster = new THREE.Raycaster();
+		this.scene = new THREE.Scene();
 
-Renderer.prototype.raycastObjects = function(position) {
-	this.raycaster.setFromCamera(position, this.camera);
-    return this.raycaster.intersectObjects(this.scene.children);
-}
+	    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000*this.scale);
+	    this.camera.position.x = 3*this.scale;
+	    this.camera.position.y = 3*this.scale;
+	    this.camera.position.z = 3*this.scale;
 
-Renderer.prototype.getCamera = function() {
-	return this.camera;
-}
+	    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+	    this.renderer.setSize(window.innerWidth, window.innerHeight);
+	    this.renderer.shadowMap.enabled = true;
+	    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-Renderer.prototype.setCamera = function(camera) {
-	this.camera = camera;
-}
+	    document.body.appendChild(this.renderer.domElement);
+	}
 
-Renderer.prototype.createOrbitControls = function() {
-	this.orbitControls = new THREE.OrbitControls(this.camera);
-    this.orbitControls.enableDamping = false;
-    this.orbitControls.maxPolarAngle = Math.PI / 2;
-}
+	destroy() {
 
-Renderer.prototype.getOrbitControls = function() {
-	return this.orbitControls;
-}
+	}
 
-Renderer.prototype.destroyOrbitControls = function() {
-	this.orbitControls = null;
-}
+	addObject(object) {
+		if (object instanceof THREE.Light) {
+			this.scene.add(object);
+		} else {
+			this.meshes.push(object.renderData.mesh);
+			this.scene.add(object.renderData.mesh);
+		}
+	}
 
-Renderer.prototype.removeAll = function() {
-	var scene = this.scene;
-	this.meshes.forEach(function (child) {
-		scene.remove(child);
-	});
-	//scene.children = [];
-}
+	removeObject(object) {
 
-Renderer.prototype.render = function() {
-	if (this.orbitControls)
-		this.orbitControls.update();
+	}
 
-	this.renderer.render(this.scene, this.camera);
+	raycastObjects(position) {
+		this.raycaster.setFromCamera(position, this.camera);
+	    return this.raycaster.intersectObjects(this.scene.children);
+	}
+
+	getCamera() {
+		return this.camera;
+	}
+
+	setCamera(camera) {
+		this.camera = camera;
+	}
+
+	createOrbitControls() {
+		this.orbitControls = new THREE.OrbitControls(this.camera);
+	    this.orbitControls.enableDamping = false;
+	    this.orbitControls.maxPolarAngle = Math.PI / 2;
+	}
+
+	getOrbitControls() {
+		return this.orbitControls;
+	}
+
+	destroyOrbitControls() {
+		this.orbitControls = null;
+	}
+
+	removeAll() {
+		var scene = this.scene;
+		this.meshes.forEach(function (child) {
+			scene.remove(child);
+		});
+		//scene.children = [];
+	}
+
+	render() {
+		if (this.orbitControls)
+			this.orbitControls.update();
+
+		this.renderer.render(this.scene, this.camera);
+	}
 }
